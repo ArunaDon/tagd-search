a2fe39509e15e34a014c18c46c2d3aacd8a85787
arunadon <nakandaladananjaya@gmail.com>
2024-11-24 20:00:37 +0100

    first commit

---
+++ src/main/java/com/tagdsearch/demo/tagd_search/TagdSearchApplication.java
@@ -0,0 +1,13 @@
+package com.tagdsearch.demo.tagd_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagdSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagdSearchApplication.class, args);
+	}
+
+}
